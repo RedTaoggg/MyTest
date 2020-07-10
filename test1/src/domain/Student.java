9e2c2353dccf51b0cdb9ca2cb69376d10359c6ec
zs <zs@bjpowernode.com>
2020-07-10 20:24:53 +0800

    test,test

---
+++ test1/src/domain/Student.java
@@ -0,0 +1,12 @@
+package domain;
+
+/**
+ * 2020年07月10日20:23
+ *
+ * @author 红桃❤
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Hello,github!!!");
+    }
+}
