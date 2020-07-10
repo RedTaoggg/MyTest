4f93a283c79c29abeeb95d79a27f1e27179b92f0
zs <zs@bjpowernode.com>
2020-07-10 19:15:23 +0800

    test2

---
+++ test2/src/domain/Person.java
@@ -0,0 +1,12 @@
+package domain;
+
+/**
+ * 2020年07月10日19:13
+ *
+ * @author 红桃❤
+ */
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
