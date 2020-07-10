c135c935e6d720dd394bd57ade2faa8cabe627da
zs <zs@bjpowernode.com>
2020-07-10 21:26:08 +0800

    又更新了

--- aaaaa/src/domain/Aaaa.java
+++ aaaaa/src/domain/Aaaa.java
@@ -10,5 +10,10 @@ public class Aaaa {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("更新了哦！！！");
+        System.out.println("更新了哦！！！");
+        System.out.println("更新了哦！！！");
+        System.out.println("更新了哦！！！");
+        System.out.println("更新了哦！！！");
+        System.out.println("更新了哦！！！");
     }
 }
