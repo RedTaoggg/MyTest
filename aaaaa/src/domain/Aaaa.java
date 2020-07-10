fa7e211635a07a9a9d86812856f7024c14607b35
zs <zs@bjpowernode.com>
2020-07-10 20:49:02 +0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ aaaaa/src/domain/Aaaa.java
@@ -0,0 +1,12 @@
+package domain;
+
+/**
+ * 2020年07月10日20:47
+ *
+ * @author 红桃❤
+ */
+public class Aaaa {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
